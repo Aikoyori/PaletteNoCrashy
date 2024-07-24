bd9885af37013706c6b9bcacf9de8e6e1a3c02e2
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2024-07-24 23:33:29 +0700

    forgor

---
+++ src/main/java/xyz/aikoyori/palettenocrashy/PaletteNoCrashy.java
@@ -0,0 +1,13 @@
+package xyz.aikoyori.palettenocrashy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PaletteNoCrashy implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
